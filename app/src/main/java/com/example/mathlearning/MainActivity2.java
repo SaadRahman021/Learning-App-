d7ca72cdc460b96879a22f5454f37339f9b010af
Saad Rahman <bsef19m021@pucit.edu.pk>
2022-05-22 01:04:18 +0500

    Add Activity Added against add button

--- app/src/main/java/com/example/mathlearning/MainActivity2.java
+++ app/src/main/java/com/example/mathlearning/MainActivity2.java
@@ -2,13 +2,29 @@ package com.example.mathlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        add = findViewById(R.id.button6);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity2.this, MainActivity3.class);
+                MainActivity2.this.startActivity(myIntent);
+            }
+        });
     }
 }
