a8640f3e3380cff1266d4345a5a1124b9086322a
Saad Rahman <bsef19m021@pucit.edu.pk>
2022-05-22 00:23:17 +0500

    Learn Activity Main Page Added

--- app/src/main/java/com/example/mathlearning/MainActivity.java
+++ app/src/main/java/com/example/mathlearning/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.mathlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button learn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        learn=findViewById(R.id.button);
+
+        learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, MainActivity2.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+
     }
 }
