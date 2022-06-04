0ba2600537f9a09f83128dc55881624c61d3148f
Saad Rahman <saad.rahman@uworx.co.uk>
2022-06-04 22:23:51 +0500

    Updated Exam Module

--- app/src/main/java/com/example/mathlearning/MainActivity7.java
+++ app/src/main/java/com/example/mathlearning/MainActivity7.java
@@ -2,11 +2,14 @@ package com.example.mathlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -28,10 +31,13 @@ public class MainActivity7 extends AppCompatActivity {
     }
     TextView text,ed;
     Button nxt,chck;
+    int a = 0;
+    int ans = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
+
 
         text=findViewById(R.id.textView);
         ed=findViewById(R.id.textView2);
@@ -39,58 +45,92 @@ public class MainActivity7 extends AppCompatActivity {
         chck=findViewById(R.id.button7);
         Random rand = new Random();
 
-        int n = rand.nextInt(100);
+
+            int n = rand.nextInt(10);
 
 
-        Random random = new Random();
+            Random random = new Random();
 
-        String setOfCharacters = "+-x";
+            String setOfCharacters = "+-x";
 
-        int randomInt = random.nextInt(setOfCharacters.length());
-        char randomChar = setOfCharacters.charAt(randomInt);
+            int randomInt = random.nextInt(setOfCharacters.length());
+            char randomChar = setOfCharacters.charAt(randomInt);
 
-        Random rand2 = new Random();
+            Random rand2 = new Random();
 
-        int num = rand2.nextInt(100);
+            int num = rand2.nextInt(10);
 
-        text.setText(Integer.toString(n)+" "+randomChar+" "+Integer.toString(num) + " = ");
+            text.setText(Integer.toString(n) + " " + randomChar + " " + Integer.toString(num) + " = ");
 
 
+            nxt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    a++;
+                    if(a==10)
+                    {
+                        Intent myIntent = new Intent(MainActivity7.this, MainActivity.class);
+                        MainActivity7.this.startActivity(myIntent);
+                         a = 0;
+                        ans = 0;
+                    }
+                    else {
+                        int n = rand.nextInt(10);
 
-        nxt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(MainActivity7.this, MainActivity7.class);
-                MainActivity7.this.startActivity(myIntent);
-            }
-        });
 
-        chck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String Ans = ed.getText().toString();
-                int x;
-                if(randomChar=='+')
-                     x = num+n;
-                else if(randomChar=='-')
-                    x=n-num;
-                else
-                    x=n*num;
+                        Random random = new Random();
 
-                if(!isInt(Ans))
-                    Ans="0";
-                if(Integer.parseInt(Ans)==x)
-                {
-                    text.setText("Right Answer");
+                        String setOfCharacters = "+-x";
+
+                        int randomInt = random.nextInt(setOfCharacters.length());
+                        char randomChar = setOfCharacters.charAt(randomInt);
+
+                        Random rand2 = new Random();
+
+                        int num = rand2.nextInt(10);
+
+                        text.setText(Integer.toString(n) + " " + randomChar + " " + Integer.toString(num) + " = ");
+
+                        String Ans = ed.getText().toString();
+                        int x;
+                        if (randomChar == '+')
+                            x = num + n;
+                        else if (randomChar == '-')
+                            x = n - num;
+                        else
+                            x = n * num;
+
+                        if (!isInt(Ans))
+                            Ans = "0";
+
+                        if (Integer.parseInt(Ans) == x) {
+                            ans++;
+                        }
+                    }
                 }
-                else
-                {
-                    text.setText("Wrong Answer");
+            });
+
+            chck.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String Ans = ed.getText().toString();
+                    int x;
+                    if (randomChar == '+')
+                        x = num + n;
+                    else if (randomChar == '-')
+                        x = n - num;
+                    else
+                        x = n * num;
+
+                    if (!isInt(Ans))
+                        Ans = "0";
+                    if (Integer.parseInt(Ans) == x) {
+                        text.setText("Right Answer");
+                    } else {
+                        text.setText("Wrong Answer");
+                    }
                 }
-            }
-        });
-
-
+            });
 
     }
 }
